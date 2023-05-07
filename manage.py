44a7ab91c0755dc841de803a56ea1ff3282e6f9c
Blopausore <augustin_650@hotmail.fr>
2023-05-07 17:32:04 +0200

    Tutorial added in README file

--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 from core.management import execute_from_command_line
 
 def main():
-
+    
     execute_from_command_line(sys.argv)
 
 
