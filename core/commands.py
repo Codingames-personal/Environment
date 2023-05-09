0f82d4a21cdbbef7774d3ab1c5605ce1000fe569
Blopausore <augustin_650@hotmail.fr>
2023-05-09 14:48:09 +0200

    cgfolder added

--- core/commands.py
+++ core/commands.py
@@ -8,25 +8,40 @@ command_list = ['merge']
 main_folder_path = __file__[:-16]
 src_path = os.path.join(main_folder_path, "src")
 
+folder_path_list = [src_path]
+
 class merge:
 
-    def script_in_order():
+    def update_folder_path_list() -> None:
+        with open(os.path.join(main_folder_path, "cgadd"), 'r') as cgadd:
+            for folder_name in cgadd.readlines():
+                folder_path_list.append(
+                    os.path.join(main_folder_path, folder_name)
+                    )
+        
+        print("take into account cgfolder")
+        
+
+    def script_in_order() -> list:
         scripts = []
-        for file_path in os.listdir(src_path):
-            if file_path[-3:] == ".py":
-                with open(os.path.join(src_path, file_path), "r") as script:
-                    first_line = script.readline()
-                    number_index = first_line.index("order :") + len("order :")
-                    number = ""
-                    while not first_line[number_index].isdigit(): number_index+=1
-                    while number_index < len(first_line) and first_line[number_index].isdigit():
-                        number+=first_line[number_index]
-                        number_index +=1
-                    if not number : 
-                        raise Exception("Not okay")
-                    
-                scripts.append({'file_path' :os.path.join(src_path, file_path), 'order' : number})
+        for folder_path in folder_path_list:
+            for file_path in os.listdir(folder_path):
+                if file_path[-3:] == ".py":
+                    with open(os.path.join(src_path, file_path), "r") as script:
+                        first_line = script.readline()
+                        number_index = first_line.index("order :") + len("order :")
+                        number = ""
+                        while not first_line[number_index].isdigit(): number_index+=1
+                        while number_index < len(first_line) and first_line[number_index].isdigit():
+                            number+=first_line[number_index]
+                            number_index +=1
+                        if not number : 
+                            raise Exception("Not okay")
+                        
+                    scripts.append({'file_path' :os.path.join(src_path, file_path), 'order' : number})
+        
         print("Order the scripts : ok", file=sys.stderr)
+        
         return list(map(
             lambda script : script['file_path'],
             sorted(
@@ -34,24 +49,30 @@ class merge:
                 key=lambda script : script['order'])
                 )
             )
+    
+
             
-    def copy_files(path_to_copy, path_to_paste):
+    def copy_files(path_to_copy : str, path_to_paste : str) -> None:
         with open(path_to_copy, "r") as copy_,  open(path_to_paste, "a") as past_:
             for line in copy_.readlines():
                 past_.write(line)
             past_.write('\n')
-            print("copy {} : ok".format(path_to_copy.split("/")[-1]), file=sys.stderr)
+            
+        print("copy {} : ok".format(path_to_copy.split("/")[-1]), file=sys.stderr)
 
 
-    def merge_scripts(scripts):
+    def merge_scripts(scripts : list) -> None:
         main_path = os.path.join(main_folder_path, "main.py")
         open(main_path, "w").close()
-        print("main.py recreated : ok", file=sys.stderr)
         for script_path in scripts:
             merge.copy_files(script_path, main_path)
 
-    def execute():
+        print("main.py recreated : ok", file=sys.stderr)
+
+
+    def execute() -> None:
         print("Starting merge", file=sys.stderr)
+        merge.update_folder_path_list()
         merge.merge_scripts(
             merge.script_in_order()
         )
