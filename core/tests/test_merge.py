928db09e84eaf6488d0c836c6e60df391c239a45
Blopausore <augustin_650@hotmail.fr>
2023-05-10 14:04:22 +0200

    Test copy files added

--- core/tests/test_merge.py
+++ core/tests/test_merge.py
@@ -17,8 +17,9 @@ class TestMerge(unittest.TestCase):
                 cgignore.write("test_file")
             self.assertTrue(merge.is_ignore("test_file"))
 
-        except :
-            del_last_line(cgignore_path)
+        except : pass
+
+        del_last_line(cgignore_path)
 
     def test_update_folder_path_list(self):
         try :
@@ -46,7 +47,27 @@ class TestMerge(unittest.TestCase):
             except : pass
             os.remove(os.path.join(src_path, file_name))
 
+
+    def test_copy_files(self):
+        test_file_path = os.path.join(os.path.abspath("."), "test_file")
+
+        with open(test_file_path, 'w') as test_file:
+            test_file.write("test")
+
+        test_file_path_copy = test_file_path + "_copy"
+        merge.copy_files(test_file_path, test_file_path_copy)
+
+        try :
+            with open(test_file_path_copy, "r") as test_file_copy:
+                self.assertEqual(test_file_copy.readline(), "test\n")    
+        except: pass
+        os.remove(test_file_path)
+        os.remove(test_file_path_copy)
+
+
         
+
+
 
 
 
